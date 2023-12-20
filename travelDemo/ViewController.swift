20aaeb289612836f16e39c7ea1e2a04637d4f0c3
liruyi <liruyiruyi26@gmail.com>
2023-12-20 16:54:01 +0800

    init proj

---
+++ travelDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travelDemo
+//
+//  Created by Ruyi Li on 2023/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ travelDemoUITests/travelDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  travelDemoUITestsLaunchTests.swift
+//  travelDemoUITests
+//
+//  Created by Ruyi Li on 2023/12/20.
+//
+
+import XCTest
+
+final class travelDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
